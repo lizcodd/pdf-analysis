e78c920aeaa82f07e147f4032bb303fbc12e5590
Liz Codd <lizcodd@gmail.com>
2021-01-01 18:20:06 -0500

    Restructure, add docstrings

--- xml2csv.py
+++ xml2csv.py
@@ -9,28 +9,71 @@ from glob import glob
 import re
 from langdetect import detect
 
-# Print usage instructions
+
 def printUsage():
+    """Prints usage message"""
+
     print("Usage: python3 xml2csv.py <input_path> <output_path> [output_filename]")
     print("e.g. To parse xmls in papers and output summary.csv in the current dir:")
     print("python3 xml2csv.py papers .")
 
-# Handle getting text - return NA for non-existant/empty elements
+
 def getTextOrNA(elt):
+    """Return the text in an element if possible
+
+    Parameters
+    ----------
+    elt : bs4.element.Tag
+        A BeautifulSoup tag element to extract text from
+
+    Returns
+    -------
+    str
+        The text within the given element or "NA" if the element is non-existant
+        or contains an empty string
+    """
+
     if elt and elt.getText():
         return elt.getText().strip()
     else:
         return "NA"
 
-# Detect the language from the element text - or return NA if non-existant/empty
+
 def getLanguage(elt):
+    """Detect the language of the text in an element if possible
+
+    Parameters
+    ----------
+    elt : bs4.element.Tag
+        A BeautifulSoup tag that contains the text to check language
+
+    Returns
+    -------
+    str
+        The detected language as a 2 letter code, or "NA" if element is
+        non-existant or contains an empty string
+    """
+
     if elt and elt.getText():
         return detect(elt.getText())
     else:
         return "NA"
 
-# Turn keywords into comma-separated string - or return NA if non-existant/empty
 def getKeywordsOrNA(keywords):
+    """Turn keywords into comma-separated string if possible
+
+    Parameters
+    ----------
+    keywords : bs4.element.Tag
+        The keywords tag that BeautifulSoup extracted from the xml
+
+    Returns
+    -------
+    str
+        All keywords separated by commas, or "NA" if element is
+        non-existant or contains an empty string
+    """
+
     if not keywords:
         return "NA"
     keywordsText = keywords.getText()
@@ -40,14 +83,39 @@ def getKeywordsOrNA(keywords):
     else:
         return "NA"
 
-# Make a guaranteed-to-exist unique ID for each xml file being processed
-# (not all papers will have DOIs if not harvested from Crossref)
 def paperID(filename):
+    """Make a guaranteed-to-exist unique ID for the xml file being processed
+
+    Parameters
+    ----------
+    filename : str
+        The name (only name, not path) of the xml file
+
+    Returns
+    -------
+    str
+        A unique ID for the file (just filename with no .xml or .tei extensions)
+    """
+
     id = re.sub('\.xml$', '', filename)  # remove .xml extensions
     id = re.sub('\.tei$', '', id)  # remove any remaining .tei extensions
     return id
 
 def parseXml(filename):
+    """Extracts some useful info (DOI, article title, etc) from xml file
+
+    Parameters
+    ----------
+    filename : str
+        The path of the xml file to be processed
+
+    Returns
+    -------
+    list
+        A list containing the info that was extracted from the xml:
+        DOI, title, publication date, keywords, abstract, language of article
+    """
+
     with open(filename, 'r') as f:
         soup = BeautifulSoup(f, 'lxml')
     record = []
@@ -60,25 +128,7 @@ def parseXml(filename):
     record.append(getLanguage(soup.abstract))
     return record
 
-### Main ###
-
-# Extract useful info from all xml files in the given directory, write to csv
-def main(in_dir, out_dir, out_filename):
-    infiles = glob(in_dir+"/*.xml")
-    outfile = open(out_dir+"/"+out_filename, 'w', encoding="UTF-8")
-    writer = csv.writer(outfile)
-
-    columns = ['ID', 'DOI', 'Title', 'Date', 'Keywords', 'Abstract', 'Language']
-    writer.writerow(columns)  # write csv header row
-
-    # For each xml file, parse info and write a row in the csv file
-    for filename in infiles:
-        record = parseXml(filename)
-        writer.writerow(record)
-
-    outfile.close()
-
-if __name__ == "__main__":
+def main():
     # Check for correct usage and initialize paths and filename
     if len(sys.argv) < 3 or len(sys.argv) > 4:
         printUsage()
@@ -89,14 +139,30 @@ if __name__ == "__main__":
     out_filename = "summary.csv"  # default output filename
 
     if len(sys.argv) == 4:
-        out_filename = sys.argv[3]
+        out_filename = sys.argv[3]  # use specified output filename
 
-    if not os.path.exists(in_dir):
+    if not os.path.exists(in_dir):  # cannot proceed if input path is wrong
         print(in_dir+" is not a valid path")
         printUsage()
         sys.exit(2)
 
-    if not os.path.exists(out_dir):
+    if not os.path.exists(out_dir):  # make output dir if doesn't already exist
         os.makedirs(out_dir)
 
-    main(in_dir, out_dir, out_filename)
+    infiles = glob(in_dir+"/*.xml")
+    outfile = open(out_dir+"/"+out_filename, 'w', encoding="UTF-8")
+    writer = csv.writer(outfile)
+
+    # Write csv header row
+    columns = ['ID', 'DOI', 'Title', 'Date', 'Keywords', 'Abstract', 'Language']
+    writer.writerow(columns)
+
+    # For each xml file, parse info and write a row in the csv file
+    for filename in infiles:
+        record = parseXml(filename)
+        writer.writerow(record)
+
+    outfile.close()
+
+if __name__ == "__main__":
+    main()
